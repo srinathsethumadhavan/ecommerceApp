7821e42f9efad5493d05aec2e5fe833d2185f547
srinathsethumadhavan <srinath15021997@gmail.com>
2020-09-01 11:52:34 +0530

    added start script and process.env.port

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.use(AdminProductsRouter);
 app.use(productsRouter)
 app.use(cartsRouter)
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT|| 3000,()=>{
     console.log("listening on port 3000")
 })
